099ea5f3e3284e2d545e164b8e360e23db0d5a84
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-30 06:09:55 -0700

    example2

---
+++ Project4.java
@@ -0,0 +1,19 @@
+package com.basics;
+
+public class Project4 {
+	public static void main(String[] args){
+		int age=100;
+		if(age == 20) {
+			System.out.println(20);
+		}
+		else
+		{
+			System.out.println("None");
+		}
+		{
+		int response = age < 200 ? 100 : 12; 
+		}
+		
+	}
+
+}
