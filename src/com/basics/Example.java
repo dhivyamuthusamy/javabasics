849de0ef43c2b9d72a8dc17407e0b10d0dd6b71f
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-29 23:49:38 -0700

    internet

---
+++ src/com/basics/Example.java
@@ -0,0 +1,13 @@
+
+package com.basics;
+
+public class Example {
+	public static void main(String[] args) {
+		int i=20;
+	if(i%2 == 0)
+		System.out.println("even");
+	else
+		System.out.println("odd");
+	}
+
+}
