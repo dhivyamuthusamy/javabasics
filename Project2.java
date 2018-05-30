9c4a514341fa4344f897d06b1ee4f795df4a8f01
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-30 02:42:21 -0700

    example1

---
+++ Project2.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class Project2 {
+  public static void main(String[] args) {
+	  int marks=45;
+	  if(marks >=40)
+		  System.out.println("good");
+	  else
+		  System.out.println("poor");
+  }
+}
