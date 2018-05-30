713090589caeaef7366a46a6709fbb8b834f25bf
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-30 02:11:16 -0700

    hai

---
+++ src/com/basics/Project.java
@@ -0,0 +1,18 @@
+package com.basics;
+
+public class Project {
+	public static void main(String[] args) {
+		int h=10;
+		switch (h) {
+		case 10:
+			System.out.println(10);
+			break;
+		case 20 :
+		System.out.println(20);
+		break;
+		case 30 :
+		System.out.println(30);
+		break;
+	}
+	}
+}
