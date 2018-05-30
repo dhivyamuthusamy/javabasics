6a81769a22baa3eb00e01738a2f88bb5ba8603cd
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-30 02:02:20 -0700

    hai

---
+++ src/com/basics/conditions.java
@@ -0,0 +1,15 @@
+package com.basics;
+
+public class conditions {
+  public static void main(String[] args) {
+		 int count=55;
+if(count < 20)
+	System.out.println(10);
+else if(count > 20)
+	System.out.println(20);
+else
+	System.out.println("none");
+	 
+  }
+
+}
